ff45f23d136e07e286cc3d85586c603ce6a43b9e
aiko <aiko@example.com>
2024-10-15 08:49:34 +0000

    非ログインTopPageとログイン後TopPageを作成

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,7 @@ use App\Http\Controllers\AuthController;
 
 //タスク管理システム
 Route::get('/', [AuthController::class, 'index']);
+Route::get('/task/list',[TaskController::class, 'list']);
 
 //テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
